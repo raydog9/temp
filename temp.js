29327e6e3ed2baa871fc77821bf5ca9b63ed9aa5
ray sonntag <raysonntag@yahoo.com>
2019-11-10 18:33:00 -0500

    reset the input field

--- temp.js
+++ temp.js
@@ -1,1 +1,20 @@
+ var a,b,c,d,e,m,n,o,r,s,t,arry=[],may=[],ray=[],say=[];
+
+ bo1=document.getElementById("bo1");
+ bo2=document.getElementById("bo2");
+ bo3=document.getElementById("bo3");
+
+  inp1.addEventListener("input",fun1); 
+
+ function fun1() {
+   var rand=Math.floor(Math.random() * 10);
+   re=inp1.value;
+   rf=inp2.value;
+  document.getElementById("myform").reset();
  
+
+ }
+ function fun2() {
+document.getElementById("myform").reset();
+
+ }
