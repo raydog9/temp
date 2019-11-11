051f84609858627fb6f2dcddd26cec670c0e6463
ray sonntag <raysonntag@yahoo.com>
2019-11-10 21:28:48 -0500

    resetting input field

--- temp.js
+++ temp.js
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
  var a,b,c,d,e,m,n,o,r,s,t,arry=[],may=[],ray=[],say=[];
 
  bo1=document.getElementById("bo1");
- bo2=document.getElementById("bo2");
- bo3=document.getElementById("bo3");
+ bo1=document.getElementById("bo1");
+ bo1=document.getElementById("bo1");
+ inp1=document.getElementById("inp1");
+inp1.addEventListener("input",fun1);
+bu1.addEventListener("click",fun2);
 
-  inp1.addEventListener("input",fun1); 
-
- function fun1() {
-   var rand=Math.floor(Math.random() * 10);
-   re=inp1.value;
-   rf=inp2.value;
-  document.getElementById("myform").reset();
- 
-
+function fun1() {
+one=inp1.value || 0 ; 
+inp1.innerHTML=one;
  }
- function fun2() {
-document.getElementById("myform").reset();
 
- }
-=======
-var a,b,c,d,e,m,n,o,r,s,t,aray=[],may=[],ray=[],say=[];
-var bo1=document.getElementById("bo1");
-var d = new Date();
-bo1.innerHTML=d;
-
-
-
- 
- 
->>>>>>> 883794e7d0e6d8fe45e9085c90d225699f908019
+function fun2() {
+    inp1.value="";
+}
