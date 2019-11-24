5126bab35cbd90f78d8bfb4535d6505bb8007ae8
ray sonntag <raysonntag@yahoo.com>
2019-11-24 04:29:02 -0500

    updated

--- temp.js
+++ temp.js
@@ -4,7 +4,6 @@
  var bo3=document.getElementById("bo3");
  var bo4=document.getElementById("bo4");
 
-<<<<<<< HEAD
  inp1.addEventListener("input",fun1);
  inp2.addEventListener("input",fun1);
  bu2.addEventListener("click",fun2);
@@ -23,32 +22,10 @@
     }
     bo3.innerHTML=a;
 
-
-
-
-
-}
-
-function fun2() {
+ function fun2() {
     bo1.innerHTML="";
     bo2.innerHTML="";
     inp1.value="";
 
 
 }
-=======
- bu1.addEventListener("click", fun1);
- bu2.addEventListener("click", fun2);
-
- function fun1() {
-     var rand=Math.floor(Math.random ()*100);
-     a=rand;
-     bo1.innerHTML=a;
-
- }
-
- function fun2() {
-     bo1.innerHTML="";
-     bo2.innerHTML="";
- }
->>>>>>> 1a7c540d90ea85c439561937cd58ed02292d46ec
