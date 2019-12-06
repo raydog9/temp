dce41adf158c50fd421d22183831fde2c6e40215
ray sonntag <raysonntag@yahoo.com>
2019-12-06 18:51:54 -0500

    120619

--- temp.js
+++ temp.js
@@ -9,13 +9,22 @@
     var mov = {
         x:Math.floor(Math.random()*1200),
         y:Math.floor(Math.random()*600),
-        mid:50
+        mid:Math.floor(Math.random() *600),
+        colr:Math.floor(Math.random() *255),
+        colg:Math.floor(Math.random() *255),
+        colb:Math.floor(Math.random() *255)
     }
+    var mov1 = {
+      x:Math.floor(Math.random()*(1200-800) +800),
+      y:Math.floor(Math.random()*600),
+      mid:Math.floor(Math.random() * 100)
+    }
+    
    
-     var rand=Math.floor(Math.random()*100);
-     fill(0,0,rand);
-     background(218,rand,200);
-     ellipse(mov.x,mov.y,mov.mid,rand);
+     var rand=Math.floor(Math.random()*255);
+     fill(mov.colr,mov.colg,mov.colb);
+      ellipse(mov.x,mov.y,mov.mid,rand);
+      ellipse(mov1.x,mov1.y,mov1.mid,rand);
 
 
    }
